0807dc181601eaa5385d3e28a062b5f1843e8565
Nico Heins <nico.heins@tutanota.com>
2022-01-20 18:33:17 +0100

    replace tabs with collapsable block

---
+++ src/components/Collapsable.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Minus from "./icons/Minus.svelte";
+    import Plus from "./icons/Plus.svelte";
+
+    export let label;
+    export let optional = true;
+    let visible = false;
+
+    function collapse() {
+        visible = !visible;
+    }
+</script>
+
+<div class="level" on:click={collapse}>
+    <div>
+        <span class="mr-2">
+            {label}
+        </span>
+        {#if optional}
+            <span class="tag is-primary is-light">Optional</span>
+        {/if}
+    </div>
+    {#if !visible}
+        <Plus />
+    {:else}
+        <Minus />
+    {/if}
+</div>
+
+<div class="px-8 py-2" style={visible ? "display: block" : "display: none"}>
+    <slot />
+</div>
+
+<hr />
--- src/components/Customer.svelte
+++ src/components/Customer.svelte
@@ -2,47 +2,36 @@
     import TableArea from "./TableArea.svelte";
     import Reservation from "./Reservation.svelte";
     import Menu from "./Menu.svelte";
+    import Collapsable from "./Collapsable.svelte";
     
 
     let showContent = 'reservation';
 </script>
 
-<div class="tabs is-centered is-medium is-boxed">
-    <ul>
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <li class:is-active="{showContent == 'reservation'}"><a on:click="{() => showContent='reservation'}">Reservierung</a></li>
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <li class:is-active="{showContent == 'menu'}"><a on:click="{() => showContent='menu'}">Speisekarte</a></li>
-    </ul>
-</div>
-<div id="tab-content">
-    {#if showContent == 'reservation'}
-    <div class="columns">
-        <div class="column is-half">
-            <Reservation/>   
-            <div class="column">
-                <div class="buttons is-right">
-                    <button class="button is-primary" on:click="{() => showContent='menu'}">
-                        <span>Weiter</span>
-                    </button>
-                </div>
+
+<div class="columns m-5">
+    <div class="column is-half">
+        <div class="box">
+            <Reservation/>
+        </div>
+        <div class="box">
+        <Collapsable
+            label="Speisekarte"
+        >
+            <Menu/>
+        </Collapsable>
+        </div>
+        <div class="column">
+            <div class="buttons is-right">
+                <button class="button is-primary" on:click="{() => alert('Vielen Dank für Ihre Reservierung, wir haben Ihre Daten erhalten!\nSie haben eine Bestätigung per E-Mail erhalten.')}">
+                    <span>Reservieren</span>
+                </button>
             </div>
         </div>
-        <div class="column is-half">
-            <TableArea isStaff=0/>
-        </div>
     </div>
-
-    {:else if showContent == 'menu'}
-    <Menu/>
-    <div class="column">
-        <div class="buttons is-right">
-            <button class="button is-primary" on:click="{() => alert('Vielen Dank für Ihre Reservierung, wir haben Ihre Daten erhalten!\nSie haben eine Bestätigung per E-Mail erhalten.')}">
-                <span>Reservieren</span>
-            </button>
-        </div>
+    <div class="column is-half">
+        <TableArea isStaff=0/>
     </div>
-    {/if}
 </div>
 
 <style>
